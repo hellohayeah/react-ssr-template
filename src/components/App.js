26d42722be77468ca684296273cedb4d3d74f92c
蔡旻勳 <hellohayeah@gmail.com>
2018-07-31 21:36:09 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './style.scss'
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello React</h1>
+  }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: { loader: 'babel-loader' }
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
