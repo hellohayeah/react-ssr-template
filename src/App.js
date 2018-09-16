d2edbbe0d83cddc57cc33104495ea3a88312b122
蔡旻勳 <hellohayeah@gmail.com>
2018-09-16 13:34:58 +0800

    add redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import './assets/style.scss'
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './assets/style.scss';
 
-import Header from './components/Header'
-import Menu from './components/Menu'
-import Home from './components/Home'
-import About from './components/About'
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
@@ -18,8 +18,8 @@ class App extends Component {
           <Route path="/about" component={About} />
         </div>
       </BrowserRouter>
-    )
+    );
   }
 }
 
-export default App
+export default App;
