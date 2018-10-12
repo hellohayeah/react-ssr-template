a9d9aa59234703de3441a49739281fae6839dac8
蔡旻勳 <hellohayeah@caiminxunde-MacBook-Pro.local>
2018-10-12 21:46:50 +0800

    FilterListWithColor ing

--- src/components/FilterListWithColor/index.js
+++ src/components/FilterListWithColor/index.js
@@ -3,10 +3,33 @@ import './style.scss'
 import items from './repos'
 
 class FilterListWithColor extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filterLists: []
+    }
+  }
+
+  inputText = e => {
+    const filterLists = items.filter(
+      item => item.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1
+    )
+
+    this.setState({ filterLists })
+  }
+
   render() {
-    return <div className="filter-list-with-color">
-    <input type="text" onChange={this.inputText}>
-    </div>
+    const { filterLists } = this.state
+    return (
+      <div className="filter-list-with-color">
+        <input type="text" onChange={this.inputText} />
+        <ul>
+          {filterLists.map((item, idx) => (
+            <li key={idx}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    )
   }
 }
 
