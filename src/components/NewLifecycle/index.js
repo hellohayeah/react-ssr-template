69bcae85a9d32662ec86ddff2af603dc86f836c0
蔡旻勳 <hellohayeah@caiminxunde-MacBook-Pro.local>
2018-10-21 00:04:21 +0800

    react doc

--- src/components/NewLifecycle/index.js
+++ src/components/NewLifecycle/index.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
 import './style.scss'
 
+import Children from './Children'
+
 class NewLifecycle extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: [],
+      comments: [],
+      date: new Date()
+    }
+  }
+
+  tick = () => {
+    this.setState({ date: new Date() })
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.timeId = setInterval(() => {
+      this.tick()
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+    clearInterval(this.tick)
+  }
+
   render() {
+    console.log('render')
+    const { date } = this.state
     return (
-    <div className="new-lifecycle">
-      NewLifecycle
-    </div>
+      <div className="new-lifecycle">
+        {date.toLocaleTimeString()}
+        <Children />
+      </div>
     )
   }
 }
