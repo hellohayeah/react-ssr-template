ac08488f8e15a4b9df9c252024eee0692cda8ad9
蔡旻勳 <hellohayeah@gmail.com>
2018-11-03 13:08:27 +0800

    portals update

--- src/components/NewLifecycle/index.js
+++ src/components/NewLifecycle/index.js
@@ -1,35 +1,16 @@
 import React, { Component } from 'react'
-
-import TemperatureInput from './TemperatureInput'
-import BoilingVerdict from './BoilingVerdict'
-import Dialog from './Dialog'
+import Portals from './Portals'
 
 export class index extends Component {
   constructor(props) {
     super(props)
-    this.textInput = null
-    this.setTextInputRef = element => {
-      this.textInput = element
-    }
-
-    this.focusTextInput = () => {
-      if (this.textInput) this.textInput.focus()
-    }
-  }
-
-  componentDidMount() {
-    this.focusTextInput()
   }
 
   render() {
     return (
-      <div>
-        <input type="text" ref={this.setTextInputRef} />
-        <input
-          type="button"
-          value="Focus the text input"
-          onClick={this.focusTextInput}
-        />
+      <div className="content">
+        ssssss
+        <Portals>123456</Portals>
       </div>
     )
   }
