ff7dd5ecea95e770bfedf54ee6e6fee3a2ac1a3d
蔡旻勳 <hellohayeah@gmail.com>
2018-11-11 13:56:10 +0800

    Render Prop update

--- src/components/RenderProp/index.js
+++ src/components/RenderProp/index.js
@@ -4,17 +4,15 @@ import Cat from './Cat'
 import './style.scss'
 
 class RenderProp extends Component {
+  renderCatImg = mouse => {
+    return <Cat mouse={mouse} />
+  }
+
   render() {
     return (
       <div className="render-prop" onMouseMove={this.handleMouseMove}>
         <h1>Move the mouse around!</h1>
-        <Mouse
-          render={mouse => (
-            <p>
-              {mouse.x}, {mouse.y}
-            </p>
-          )}
-        />
+        <Mouse render={this.renderCatImg} />
       </div>
     )
   }
