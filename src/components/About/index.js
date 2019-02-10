396d7d485025c5f15437b21e30aed183fb71137e
蔡旻勲 <hellohayeah@gmail.com>
2019-02-10 15:00:41 +0800

    react update add hook

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { AboutStyle } from './style'
 
+import UseAbout from './useAbout'
+
 class About extends Component {
   render() {
     return (
       <AboutStyle>
         <h2>About</h2>
+        <UseAbout />
       </AboutStyle>
     )
   }
