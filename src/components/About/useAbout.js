36b621c6b68fc50c100afd818093cf2d8448ee8b
蔡旻勳 <hellohayeah@gmail.com>
2019-02-11 10:16:06 +0800

    hooks test

--- src/components/About/useAbout.js
+++ src/components/About/useAbout.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const useAbout = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(100)
+  const [name, setName] = useState('Hooks')
+  const [todos, setTodos] = useState([{ id: 0, text: 'Learn Hooks' }])
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+    console.log(`更新後${count}`)
+  }, [count])
 
   return (
     <div>
       <p>You clicked {count} times</p>
+      <p>{name}</p>
+      {todos.map(todo => (
+        <p key={todo.id}>{todo.text}</p>
+      ))}
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   )
