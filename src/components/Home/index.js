3b22fb91ac59d3e7836a64f76598cf804af9b774
蔡旻勳 <hellohayeah@gmail.com>
2019-03-24 16:14:09 +0800

    fix bug

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
 import { HomeStyle } from './style'
 
-import Container1 from '../../containers/Container1'
-
 class Home extends Component {
   render() {
-    return (
-      <HomeStyle>
-        <Container1 />
-      </HomeStyle>
-    )
+    return <HomeStyle>Home</HomeStyle>
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store'
 import App from './App'
 
 render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 )
