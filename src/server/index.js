86df563d083b0c6da9abb36c74361402d0ae7198
蔡旻勳 <hellohayeah@gmail.com>
2019-07-19 20:23:08 +0800

    chunk

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,38 @@
-import express from 'express'
+// import express from 'express'
 import path from 'path'
-import cors from 'cors'
-import bodyParser from 'body-parser'
+// import cors from 'cors'
+// import bodyParser from 'body-parser'
 import manifestHelpers from 'express-manifest-helpers'
 import paths from '../../utils/paths'
 import render from './render'
 
+// const app = express()
+// const port = 3000
+
+// import webpack from 'webpack'
+// import middleware from 'webpack-dev-middleware'
+// import webpackHotMiddleware from 'webpack-hot-middleware'
+// import clientConfig from '../../config/client.dev'
+
+// const compiler = webpack(clientConfig)
+
+// app.use(cors)
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+// app.use(paths.publicPath, express.static(paths.clientBuild))
+// app.use(middleware(compiler))
+// app.use(webpackHotMiddleware(compiler))
+
+// app.use(render())
+
+// app.listen(port, () => {
+//   console.log(`React SSR app is running at http://localhost:${port}/`)
+// })
+import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import App from '../common/App'
 const app = express()
 const port = 3000
 
@@ -16,21 +43,15 @@ import clientConfig from '../../config/client.dev'
 
 const compiler = webpack(clientConfig)
 
-app.use(cors)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use('/', express.static(paths.clientBuild))
+app.use(paths.publicPath, express.static(paths.clientBuild))
 app.use(middleware(compiler))
 app.use(webpackHotMiddleware(compiler))
-
 const manifestPath = path.join(paths.clientBuild, paths.publicPath)
 app.use(
   manifestHelpers({
     manifestPath: `${manifestPath}/manifest.json`
   })
 )
-
 app.use(render())
 
 app.listen(port, () => {
