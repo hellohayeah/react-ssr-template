3a7f8828aaf91f66c7196f37000b7830cec46589
蔡旻勲 <jerrytsai@eztravel.com.tw>
2020-12-03 11:24:16 +0800

    refactor: 更換路徑

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import Home from '../common/pages/Home'
-import About from '../common/pages/About'
-
-const routes = [
-  { path: '/', exact: true, component: Home },
-  { path: '/about', exact: true, component: About }
-]
-
-export default routes
